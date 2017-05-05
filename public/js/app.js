d9cc79f2e4f330ed9f3972fe5a9e69abd0378a1f
Julian Affrime <julian.affrime@gmail.com>
2017-05-05 21:42:53 +0300

    bootstrap & ui-router running

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
 var app = angular.module('learnapp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/templates/home.html',
+      // controller: 'mainCtrl',
+      // resolve: {
+      //   myPosts: function($http) {
+      //     return $http.get('/posts');
+      //   }
+      // }
+    })
+
+  $urlRouterProvider.otherwise('home');
+
+});
--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+});
+
 app.listen(8000, function(){
   console.log("LearnApp up and running on 8000");
 })
