1bcff083081792c72458df4232ed5d2bc08c454c
Julian Affrime <julian.affrime@gmail.com>
2017-05-05 20:19:16 +0300

    basic structure

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
+// var expressSession = require('express-session');
+
+// mongoose.connect('mongodb://localhost/learnapp');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
