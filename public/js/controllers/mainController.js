fbd333b9ffb265328ea73890aac87a27a4e96f15
Julian Affrime <julian.affrime@gmail.com>
2017-05-07 19:21:56 +0300

    basic grid and info... can be edited / deleted

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -2,4 +2,6 @@ app.controller('mainCtrl', function($scope) {
 
   $scope.test = "This is a test."
 
+  $scope.languagesArray = ["HTML", "CSS", "Javascript", "Python", "etc."];
+
 }); //controller end
